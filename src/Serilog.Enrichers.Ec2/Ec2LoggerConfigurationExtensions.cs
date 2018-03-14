4f0989713b0b4986d413f1376c65aeeae8fde558
Mark Leybourne <Mark.Leybourne@interact-intranet.com>
2018-03-14 13:43:46 +0000

    Initial commit

---
+++ src/Serilog.Enrichers.Ec2/Ec2LoggerConfigurationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Serilog.Configuration;
+using Serilog.Enrichers;
+
+namespace Serilog
+{
+    /// <summary>
+    /// Extends <see cref="LoggerConfiguration"/> to add enrichers for AWS EC2.
+    /// </summary>
+    public static class Ec2LoggerConfigurationExtensions
+    {
+		/// <summary>
+		/// Enrich log events with an Ec2InstanceId property containing the current EC2 instance Id.
+		/// </summary>
+		/// <param name="enrichmentConfiguration">Logger enrichment configuration.</param>
+		/// <returns>Configuration object allowing method chaining.</returns>
+		public static LoggerConfiguration WithEc2InstanceId(
+           this LoggerEnrichmentConfiguration enrichmentConfiguration)
+        {
+            if (enrichmentConfiguration == null)
+				throw new ArgumentNullException(nameof(enrichmentConfiguration));
+
+            return enrichmentConfiguration.With<Ec2InstanceIdEnricher>();
+        }
+    }
+}
---
+++ src/Serilog.Enrichers.Ec2/Enrichers/Ec2InstanceIdEnricher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Serilog.Core;
+using Serilog.Events;
+using System.Net.Http;
+
+namespace Serilog.Enrichers
+{
+	/// <summary>
+	/// Enriches log events with an Ec2InstanceId property containing the current EC2 instance Id.
+	/// </summary>
+	public class Ec2InstanceIdEnricher : ILogEventEnricher
+	{
+		private const string InstanceMetadataUrl = "http://169.254.169.254/latest/meta-data";
+
+		private LogEventProperty _cachedProperty;
+
+		/// <summary>
+		/// The property name added to enriched log events.
+		/// </summary>
+		public const string Ec2InstanceIdPropertyName = "Ec2InstanceId";
+
+		/// <summary>
+		/// Enrich the log event.
+		/// </summary>
+		/// <param name="logEvent">The log event to enrich.</param>
+		/// <param name="propertyFactory">Factory for creating new properties to add to the event.</param>
+		public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+		{
+			string ec2InstanceId = GetEc2InstanceId();
+
+			_cachedProperty = _cachedProperty ?? propertyFactory.CreateProperty(Ec2InstanceIdPropertyName, ec2InstanceId);
+
+			logEvent.AddPropertyIfAbsent(_cachedProperty);
+		}
+
+		private string GetEc2InstanceId()
+		{
+			return new HttpClient()
+				.GetStringAsync(InstanceMetadataUrl + "/instance-id")
+				.GetAwaiter()
+				.GetResult();
+		}
+	}
+}
