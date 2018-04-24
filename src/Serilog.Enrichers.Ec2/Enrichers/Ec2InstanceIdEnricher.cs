52d1fdf9e1c726183fa6775034f48efaa42329b3
Mark Leybourne <Mark.Leybourne@interact-intranet.com>
2018-04-24 10:04:12 +0100

    Use "Unavailable" for instance Id if failed to obtain

--- src/Serilog.Enrichers.Ec2/Enrichers/Ec2InstanceIdEnricher.cs
+++ src/Serilog.Enrichers.Ec2/Enrichers/Ec2InstanceIdEnricher.cs
@@ -6,15 +6,18 @@ using System.Net.Http;
 namespace Serilog.Enrichers
 {
 	/// <summary>
-	/// Enriches log events with an EC2-INSTANCE-ID property containing the current EC2 instance Id.
+	/// Enriches log events with an Ec2InstanceId property containing the current EC2 instance Id.
 	/// </summary>
 	public class Ec2InstanceIdEnricher : ILogEventEnricher
 	{
-		private const string Ec2InstanceIdPropertyName = "EC2-INSTANCE-ID";
+		private const string InstanceMetadataUrl = "http://169.254.169.254/latest/meta-data";
 
 		private LogEventProperty _cachedProperty;
 
-		private string _ec2InstanceId;
+		/// <summary>
+		/// The property name added to enriched log events.
+		/// </summary>
+		public const string Ec2InstanceIdPropertyName = "Ec2InstanceId";
 
 		/// <summary>
 		/// Enrich the log event.
@@ -23,14 +26,30 @@ namespace Serilog.Enrichers
 		/// <param name="propertyFactory">Factory for creating new properties to add to the event.</param>
 		public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
 		{
-			if (_ec2InstanceId == null)
-			{
-				_ec2InstanceId = Ec2InstanceMetadata.GetProperty("/instance-id");
-			}
+			string ec2InstanceId = GetEc2InstanceId();
 
-			_cachedProperty = _cachedProperty ?? propertyFactory.CreateProperty(Ec2InstanceIdPropertyName, _ec2InstanceId);
+			_cachedProperty = _cachedProperty ?? propertyFactory.CreateProperty(Ec2InstanceIdPropertyName, ec2InstanceId);
 
 			logEvent.AddPropertyIfAbsent(_cachedProperty);
+		}
+
+		private string GetEc2InstanceId()
+		{
+			string instanceId;
+
+			try
+			{
+				instanceId = new HttpClient()
+					.GetStringAsync(InstanceMetadataUrl + "/instance-id")
+					.GetAwaiter()
+					.GetResult();
+			}
+			catch
+			{
+				instanceId = "Unavailable";
+			}
+
+			return instanceId;
 		}
 	}
 }
